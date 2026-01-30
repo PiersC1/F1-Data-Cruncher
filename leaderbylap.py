96bf348f333803de307130df96d637f7fa9304f5
Piers Chatham <pierschatham@gmail.com>
2026-01-30 11:42:40 -0800

    Created a points simulation that awards points at the end of every lap to better understand consistency across the season

---
+++ leaderbylap.py
@@ -0,0 +1,68 @@
+import fastf1
+import pandas as pd
+import math
+
+
+
+
+def pretty_print(dr):
+    for d in dr:
+        print(f"Driver: {number_name[d]} Points: {dr[d]}")
+
+year = 2025
+points_by_pos = {
+    1 : 25,
+    2 : 18,
+    3 : 15,
+    4 : 12,
+    5 : 10,
+    6 : 8,
+    7 : 5,
+    8 : 4,
+    9 : 2,
+    10 : 1,
+    11 : 0,
+    12 : 0,
+    13 : 0,
+    14 : 0,
+    15 : 0,
+    16 : 0,
+    17 : 0,
+    18 : 0,
+    19 : 0,
+    20 : 0
+}
+
+number_name = {}
+drivers = {} # 'driverName' : points (int)
+
+eventSchedule = fastf1.get_event_schedule(year)
+
+events = pd.DataFrame(eventSchedule)
+
+for _, row in events.iterrows():
+    if row['RoundNumber'] == 0:
+        continue # Filter out testing sessions
+    event = eventSchedule.get_event_by_round(row['RoundNumber'])
+    race = event.get_race()
+    race.load()
+    # Updating Driver Roster
+    keys = drivers.keys()
+    for driver in race.drivers:
+        if driver not in keys:
+            drivers[driver] = 0
+    keys = drivers.keys()
+    laps = race.laps
+    for driver in keys:
+        driver_laps = laps.pick_drivers(driver)
+        for _, lap in driver_laps.iterlaps():
+            if driver not in number_name.keys():
+                number_name[driver] = lap['Driver']
+            if not math.isnan(lap['Position']):
+                drivers[driver] += points_by_pos[int(lap['Position'])]
+
+sorted_drivers = dict(sorted(drivers.items(), key=lambda item: item[1], reverse=True))
+
+
+
+pretty_print(sorted_drivers)
